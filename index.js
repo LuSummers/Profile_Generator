90504900d2604fb898d97981939017578b50afa0
Lu Summers <lusummers@users.noreply.github.com>
2022-05-13 01:23:16 -0400

    functioning app

--- index.js
+++ index.js
@@ -2,104 +2,109 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
+const generateMarkdown = require('./src/generateMarkdown')
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const team = [];
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html")
 
-
-const mainMenu = () => {
-  inquirer.prompt(
-    {
-      type: 'list',
-      name: 'type',
-      message: 'what type of employee',
-      choices: ['Engineer', 'Intern', 'Manager', 'Build Team']
-    }
-  ).then(answers => {
-    if (answers.type === 'Engineer') {
-      //call engineer questions
-      promptEngineer();
-
-    } else if (answers.type === 'Manager') {
-      //call Manager questions
-      promptManager();
-
-    } else if (answers.type === 'Intern') {
-      //call intern questions
-      promptIntern();
-
-    } else {
-      //run HTML generator function
-    }
-  })
-};
-// TODO: Create an array of questions for user input
-
-//MANAGER
-const promptManager = () => {
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name? (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your name!');
-          return false;
-        }
+const runApp = () => {
+  const mainMenu = () => {
+    inquirer.prompt(
+      {
+        type: 'list',
+        name: 'type',
+        message: 'what type of employee',
+        choices: ['Engineer', 'Intern', 'Manager', 'Build Team']
       }
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: 'Enter your id # (Required)',
-      validate: idInput => {
-        if (idInput) {
-          return true;
-        } else {
-          console.log('Please enter your id#!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email? (Required)',
-      validate: emailInput => {
-        if (emailInput) {
-          return true;
-        } else {
-          console.log('Please enter your email!');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'NUMBER',
-      message: 'What is your OFFICE NUMBER? (Required)',
-      validate: numberInput => {
-        if (numberInput) {
-          return true;
-        } else {
-          console.log('Please enter OFFICE NUMBER!');
-          return false;
-        }
-      }
-    },
+    ).then(answers => {
+      if (answers.type === 'Engineer') {
+        //call engineer questions
+        promptEngineer();
 
-  ]).then(answers => {
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.NUMBER);
-    team.push(manager);
-    mainMenu();
-  })
+      } else if (answers.type === 'Manager') {
+        //call Manager questions
+        promptManager();
 
-};
+      } else if (answers.type === 'Intern') {
+        //call intern questions
+        promptIntern();
+
+      } else {
+        //run HTML generator function
+        buildTeam();
+      }
+    })
+  };
+  // An array of questions for user input
+
+  //MANAGER
+  const promptManager = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your id # (Required)',
+        validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log('Please enter your id#!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'NUMBER',
+        message: 'What is your OFFICE NUMBER? (Required)',
+        validate: numberInput => {
+          if (numberInput) {
+            return true;
+          } else {
+            console.log('Please enter OFFICE NUMBER!');
+            return false;
+          }
+        }
+      },
+
+    ]).then(answers => {
+      console.log(answers);
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.NUMBER);
+      team.push(manager);
+      mainMenu();
+    })
+
+  };
   //ENGINEER
   const promptEngineer = () => {
     inquirer.prompt([
@@ -156,74 +161,88 @@ const promptManager = () => {
         }
       },
     ]).then(answers => {
+      console.log(answers);
       const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
       team.push(engineer);
       mainMenu();
-    })
+    });
   }
-    //INTERN
-    const promptIntern = () => {
-      inquirer.prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is your name? (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('Please enter your name!');
-              return false;
-            }
+  //INTERN
+  const promptIntern = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
           }
-        },
-        {
-          type: 'input',
-          name: 'id',
-          message: 'Enter your id # (Required)',
-          validate: idInput => {
-            if (idInput) {
-              return true;
-            } else {
-              console.log('Please enter your id# !');
-              return false;
-            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your id # (Required)',
+        validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log('Please enter your id# !');
+            return false;
           }
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: 'What is your email? (Required)',
-          validate: emailInput => {
-            if (emailInput) {
-              return true;
-            } else {
-              console.log('Please enter your email!');
-              return false;
-            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? (Required)',
+        validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log('Please enter your email!');
+            return false;
           }
-        },
-        {
-          type: 'input',
-          name: 'school',
-          message: 'What school do you attend? (Required)',
-          validate: schoolInput => {
-            if (schoolInput) {
-              return true;
-            } else {
-              console.log('Please enter your school.');
-              return false;
-            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'What school do you attend? (Required)',
+        validate: schoolInput => {
+          if (schoolInput) {
+            return true;
+          } else {
+            console.log('Please enter your school.');
+            return false;
           }
-        },
+        }
+      },
 
-      ]).then(answers => {
-        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-        team.push(intern);
-        mainMenu();
-      })
+    ]).then(answers => {
+      console.log(answers);
+      const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+      team.push(intern);
+      mainMenu();
+    });
+  }
 
-    }
+  const buildTeam = () => {
+    console.log(`finished building team`);
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+  fs.writeFileSync(outputPath, generateMarkdown(team), "UTF-8");
 
-    mainMenu();
-   
+}
+
+mainMenu();
+}
+  
+
+runApp();
+
---
+++ src/generateMarkdown.js
@@ -0,0 +1,106 @@
+const generateTeam = team => {
+    console.log(team);
+
+
+
+        const generateManager = manager => 
+        {
+            console.log(manager);
+            return `
+            <div class="card">
+            <div class="card-header">${manager.name}</div>
+                <card-title class="manager"><i class='fa fa-coffee'></i>Manager</card-title>
+                <ul class ="list-group">
+                    <li class="list-group-item">ID: ${manager.id}</li>
+                    <li class="list-group-item">Email: <span id = "email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+                    <li class="list-group-item">Office Number ${manager.officeNumber}</li>
+                </ul>
+            </div></card>`;
+           
+        };
+        const generateEngineer = engineer => 
+        {
+            console.log(engineer);
+            return `
+            <div class="card">
+            <div class="card-header">${engineer.name}</div>
+            <card-title class="engineer"><i class='fas fa-glasses'></i> Engineer</card-title>
+            <ul class ="list-group">
+                <li class="list-group-item">ID: ${engineer.id}</li>
+                <li class="list-group-item">Email: <span id = "email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+                <li class="list-group-item">Github:${engineer.github}</li>
+            </ul>
+        </div></card>`;
+            
+        };
+        const generateIntern = intern =>
+        {
+            console.log(intern);
+            return `
+            <div class="card">
+            <div class="card-header">${intern.name}</div>
+            <card-title class="intern"><i class='fa fa-graduation-cap'></i>Intern</card-title> 
+            <ul class ="list-group">
+                <li class="list-group-item">ID: ${intern.id}</li>
+                <li class="list-group-item">Email: <span id = "email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+                <li class="list-group-item">School:${intern.school}</li>
+            </ul>
+        </div></card>`;
+        }
+
+        const html = [];
+
+        html.push(team
+            .filter(employee => employee.getRole() === "Manager")
+            .map(manager => generateManager(manager))
+        );
+        html.push(team
+            .filter(employee => employee.getRole() === "Engineer")
+            .map(engineer => generateEngineer(engineer))
+            .join("")
+        );
+        html.push(team
+            .filter(employee => employee.getRole() === "Intern")
+            .map(intern => generateIntern(intern))
+            .join("")
+        );
+    
+        return html.join("");
+    
+    }
+
+
+
+// for (let i =0; i<team.length; i++){
+//     if (const generateManager:(manager:any) => 
+//     generateManager(team[i]);}
+//     if team[i].getRole()==="Engineer"){
+//         generateEngineer(team[i]);
+//     }
+//     if team[i].getRole()==="Intern"){
+//         generateIntern(team[i]);
+// }
+
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> 
+        <link rel="stylesheet" href="/style.css">
+        <title>TEAMS</title>
+    </head>
+    <body>
+        <header>
+        <h1>My Team</h1></header>
+    <main>${generateTeam(team)}</main>
+        
+    </body>
+        
+    </body>
+    </html>`;
+}
+
